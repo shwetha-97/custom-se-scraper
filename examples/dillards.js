65843c66c6cd94e04437a96b7fa23fb3add2f381
shwetha-97 <42267522+shwetha-97@users.noreply.github.com>
2019-07-05 12:43:28 +0530

    Add files via upload

---
+++ examples/dillards.js
@@ -0,0 +1,25 @@
+const se_scraper = require('./../src/node_scraper.js');
+
+(async () => {
+    let browser_config = {
+        headless: true,
+        debug_level: 1,
+        output_file: 'examples/results/dillards.json',
+        dillards_settings: {
+            amazon_domain: 'dillards.com',
+        }
+    };
+
+    let scrape_job = {
+        search_engine: 'dillards',
+        keywords: ['shirt', 'dress'],
+        num_pages: 1,
+    };
+
+    var scraper = new se_scraper.ScrapeManager(browser_config);
+    await scraper.start();
+
+    var results = await scraper.scrape(scrape_job);
+    console.dir(results, {depth: null, colors: true});
+    await scraper.quit();
+})();
